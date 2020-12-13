f6afbc8cc0eef3cb29fc6c5e3cc03bfcf9d8e0cd
ElzaCS <anuejohn@gmail.com>
2020-12-13 23:30:54 +0530

    Navigator changes

--- app/Navigator.js
+++ app/Navigator.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
+import Analysis from '../Pages/Analysis';
 
 const Stack = createStackNavigator();
 
 function MainStackNavigator() {
     return (
         <Stack.Navigator>
-            
+           <Stack.Screen
+            name="Analysis"
+            component={Analysis}
+            options={{ title: 'Analysis' }}
+        /> 
         </Stack.Navigator>
     );
 }
