81cf571cf0ea395155ae0afdb51f85c707fd3036
Jayashrri <balaji.jayashrri@gmail.com>
2020-12-13 22:08:44 +0530

    Add Navigator

---
+++ app/Navigator.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
+function MainStackNavigator() {
+    return (
+        <Stack.Navigator>
+            
+        </Stack.Navigator>
+    );
+}
+
+export default MainStackNavigator;
