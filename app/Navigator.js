63a1befede6ea6f3897936ccd24699975e098cff
pavi114 <pavisri99@gmail.com>
2020-12-14 20:23:48 +0530

    feat: landing template

--- app/Navigator.js
+++ app/Navigator.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
-import Analysis from '../Pages/Analysis';
+import Analysis from './Pages/Analysis';
+import Landing from './Pages/Landing';
 
 const Stack = createStackNavigator();
 
 function MainStackNavigator() {
     return (
         <Stack.Navigator>
-           <Stack.Screen
+            <Stack.Screen
+                name="Landing"
+                component={Landing}
+                options={{ headerShown: false }}
+            />
+           {/* <Stack.Screen
             name="Analysis"
             component={Analysis}
             options={{ title: 'Analysis' }}
-        /> 
+        />  */}
         </Stack.Navigator>
     );
 }
---
+++ app/Pages/Landing.js
@@ -0,0 +1,69 @@
+import React from 'react';
+
+import { Button, Image, ImageBackground, StyleSheet, Text, TouchableHighlight, View} from 'react-native';
+
+function Landing() {
+    return (
+        <ImageBackground source={require('../assets/home_bg.png')} style={styles.imgBackground}>
+                <Image source={require('../assets/home_clouds.png')} style={styles.imgCloud}></Image>
+                <Text style={styles.header}>Welcome Back, <Text style={{color: '#ff0000'}}>blah blah</Text></Text>
+                <Image source={require('../assets/red_dino.png')} style={styles.imgDino}></Image>
+                <TouchableHighlight
+                    style={styles.button}
+                    underlayColor='#8a2be2'>
+                        <Text>Do Something</Text>
+                </TouchableHighlight>
+        </ImageBackground>
+    );
+}
+
+const styles = StyleSheet.create({
+    imgBackground: {
+        width: null,
+        height: null,
+        flex: 1 ,
+        resizeMode: 'cover'
+    }, 
+    imgCloud: {
+        position: 'absolute',
+        marginTop: '0.5%',
+        alignSelf: 'center',
+        height: '100%',
+        width: '100%',
+        resizeMode: 'contain'
+    },
+    imgDino: {
+        position: 'absolute',
+        top: 0,
+        alignSelf: 'center',
+        height: '80%',
+        width: '65%',
+        resizeMode: 'contain'
+    },
+    header: {
+        color: '#000',
+        position: 'absolute',
+        textAlign: 'center',
+        width: '100%',
+        top: '21%',
+        height: 'fit-content',
+        textTransform: 'uppercase',
+        fontSize: '20px'
+    },
+    button: {
+        position: 'absolute',
+        top: '70%',
+        alignItems: 'center',
+        left: '30%',
+        padding: '10px',
+        fontSize: '20px',
+        borderWidth: '2px',
+        borderRadius: '7px',
+        borderColor: '#000',
+        color: '#fff',
+        textTransform: 'uppercase',
+        backgroundColor: '#BA55D3c2'
+    }
+})
+    
+export default Landing;
