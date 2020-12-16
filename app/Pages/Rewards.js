40a05916776eec17c3fa847034d7c75e6b2ae96c
PriyanshuDangi <priyanshudangi37@gmail.com>
2020-12-17 02:50:52 +0530

    fix: rewards scrollview

--- app/Pages/Rewards.js
+++ app/Pages/Rewards.js
@@ -1,99 +1,100 @@
 import React from 'react';
-import {StyleSheet, View, Text, Image, ScrollView} from 'react-native';
-// import { ScrollView } from 'react-native-gesture-handler';
+import { StyleSheet, Text, SafeAreaView, ScrollView, Image, View } from 'react-native';
+import Constants from 'expo-constants';
 
-function Rewards({navigation}){
-    return (
-        <View style={styles.container}>
-            <View style={styles.header}>
+function Rewards({navigation}) {
+  return (
+    <View style={styles.container}>
+        <View style={styles.header}>
                 <Text style={styles.headerText}>Rewards</Text>
-            </View>
-            <View style={styles.boxContainer}>
-                <ScrollView contentContainerStyle={styles.box}>
-                    <View style={styles.imgContainer}>
-                        <Image source={require('../assets/red_dino.png')} style={styles.rewardsImage}></Image>
-                        <Text style={styles.imgLabel}>Reward 1</Text>
-                    </View>
-                    <View style={styles.imgContainer}>
-                        <Image source={require('../assets/red_dino.png')} style={styles.rewardsImage}></Image>
-                        <Text style={styles.imgLabel}>Reward 1</Text>
-                    </View>
-                    <View style={styles.imgContainer}>
-                        <Image source={require('../assets/red_dino.png')} style={styles.rewardsImage}></Image>
-                        <Text style={styles.imgLabel}>Reward 1</Text>
-                    </View>
-                    <View style={styles.imgContainer}>
-                        <Image source={require('../assets/red_dino.png')} style={styles.rewardsImage}></Image>
-                        <Text style={styles.imgLabel}>Reward 1</Text>
-                    </View><View style={styles.imgContainer}>
-                        <Image source={require('../assets/red_dino.png')} style={styles.rewardsImage}></Image>
-                        <Text style={styles.imgLabel}>Reward 1</Text>
-                    </View>
-                    <View style={styles.imgContainer}>
-                        <Image source={require('../assets/red_dino.png')} style={styles.rewardsImage}></Image>
-                        <Text style={styles.imgLabel}>Reward 1</Text>
-                    </View>
-                    <View style={styles.imgContainer}>
-                        <Image source={require('../assets/red_dino.png')} style={styles.rewardsImage}></Image>
-                        <Text style={styles.imgLabel}>Reward 1</Text>
-                    </View>
-                </ScrollView>
-            </View>
         </View>
-    )
+      <ScrollView style={styles.scrollView}>
+          <View style={styles.box}>
+        <View style={styles.imgContainer}>
+            <Image source={require('../assets/red_dino.png')} style={styles.rewardsImage}></Image>
+            <Text style={styles.imgLabel}>Reward 1</Text>
+        </View>
+        <View style={styles.imgContainer}>
+            <Image source={require('../assets/red_dino.png')} style={styles.rewardsImage}></Image>
+            <Text style={styles.imgLabel}>Reward 1</Text>
+        </View><View style={styles.imgContainer}>
+            <Image source={require('../assets/red_dino.png')} style={styles.rewardsImage}></Image>
+            <Text style={styles.imgLabel}>Reward 1</Text>
+        </View>
+        <View style={styles.imgContainer}>
+            <Image source={require('../assets/red_dino.png')} style={styles.rewardsImage}></Image>
+            <Text style={styles.imgLabel}>Reward 1</Text>
+        </View><View style={styles.imgContainer}>
+            <Image source={require('../assets/red_dino.png')} style={styles.rewardsImage}></Image>
+            <Text style={styles.imgLabel}>Reward 1</Text>
+        </View>
+        <View style={styles.imgContainer}>
+            <Image source={require('../assets/red_dino.png')} style={styles.rewardsImage}></Image>
+            <Text style={styles.imgLabel}>Reward 1</Text>
+        </View><View style={styles.imgContainer}>
+            <Image source={require('../assets/red_dino.png')} style={styles.rewardsImage}></Image>
+            <Text style={styles.imgLabel}>Reward 1</Text>
+        </View>
+        <View style={styles.imgContainer}>
+            <Image source={require('../assets/red_dino.png')} style={styles.rewardsImage}></Image>
+            <Text style={styles.imgLabel}>Reward 1</Text>
+        </View><View style={styles.imgContainer}>
+            <Image source={require('../assets/red_dino.png')} style={styles.rewardsImage}></Image>
+            <Text style={styles.imgLabel}>Reward 1</Text>
+        </View>
+        </View>
+      </ScrollView>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        flexDirection: "column",
-        backgroundColor: 'blue'
+        marginTop: Constants.statusBarHeight,
+        backgroundColor: 'white'
     },
     header: {
-        flex:1,
-        justifyContent: 'center',
-        alignContent: 'center',
+        justifyContent: 'space-evenly',
         width: '100%',
-        height: '20%',
-        textAlign: 'center',
+        height: '15%',
+        // paddingTop: '7%',
     },
     headerText: {
         fontSize: 48,
         fontWeight: '700',
-        color: '#ffffff'
+        color: '#000000',
+        textAlign: 'center',
     },
-    boxContainer: {
-        flex: 4,
-        borderTopWidth: 1,
-        borderTopLeftRadius: 24,
-        borderTopRightRadius: 24,
-        paddingTop: 10,
-        backgroundColor: '#fff'
+    scrollView: {
+        flexDirection: 'column',
+        backgroundColor: 'white',
+        // marginHorizontal: 20,
     },
     box: {
         flex: 1,
         flexDirection: 'row',
         flexWrap: 'wrap',
         justifyContent: 'space-evenly',
-        // alignContent: 'space-around',
-        position: 'relative'
-        // paddingTop: 10
     },
     imgContainer: {
         width: '40%',
-        height: '35%',
+        // height: '35%',
         marginTop: '3%',
         marginBottom: '3%',
         textAlign: 'center',
     },
     rewardsImage: {
-        width: '100%',
-        height: '80%',
+        // width: '100%',
+        // height: '80%',
         borderWidth: 1,
         borderRadius: 10,
         marginBottom: 5,
         backgroundColor: '#e5e5e5'
+    },
+    imgLabel: {
+        textAlign: 'center'
     }
-})
+});
 
 export default Rewards;
